906994781ba336c1e36c97ae9cf0186efd39a2f6
{ Mattyu's Web } <62568369+MattyuCode@users.noreply.github.com>
2022-09-13 23:53:33 -0600

    Numeros en Chuj

---
+++ app.js
@@ -0,0 +1,195 @@
+const select = document.querySelectorAll('.listados select'),
+    input = document.querySelector('#texto'),
+    alertDanger = document.querySelector('.alDanger'),
+    form = document.querySelector('form');
+
+// const numerosChuj = {
+//     1: "Jun",
+//     2: "Cha’ab’",
+//     3: "Oxe’",
+//     4: "Chanhe’",
+//     5: "Hoye’",
+//     6: "Wake’",
+//     7: "Huke’",
+//     8: "Wajxake’",
+//     9: "B’alunhe’",
+//     10: "Lajunhe’",
+//     11: "Huxluche'",
+//     12: "Lajchawe'",
+//     13: "Oxlajunhe'",
+//     14: "Chanhlajunhe'",
+//     15: "Holajunhe'",
+//     16: "Waklajunhe'",
+//     17: "Huklajunhe'",
+//     18: "Wajxaklajunhe'",
+//     19: "B’Alunhlajunhe'",
+//     20: "Junwinak",
+//     21: "Jun xchawinak",
+//     22: "Chab’ xchawinak",
+//     23: "Oxe xchawinak",
+//     24: "Chanhe xchawinak",
+//     25: "Hoye’ xchawinak",
+//     26: "Wake xchawinak",
+//     27: "Huke xchawinak",
+//     28: "Wajxake xchawinak",
+//     29: "B’alunhe xchawinak",
+//     30: "Lajunhe'xchawinak"
+// }
+
+const numerosChujEnLetras = [{
+        id: 1,
+        name: "Ju'un"
+    },
+    {
+        id: 2,
+        name: "Cha’ab’"
+    },
+    {
+        id: 3,
+        name: "Oxe’"
+    },
+    {
+        id: 4,
+        name: "Chanhe’"
+    },
+    {
+        id: 5,
+        name: "Hoye’"
+    },
+    {
+        id: 6,
+        name: "Wake’"
+    },
+    {
+        id: 7,
+        name: "Huke’"
+    },
+    {
+        id: 8,
+        name: "Wajxake’"
+    },
+    {
+        id: 9,
+        name: "B’alunhe’"
+    },
+    {
+        id: 10,
+        name: "Lajunhe’"
+    },
+    {
+        id: 11,
+        name: "Huxluche'"
+    },
+    {
+        id: 12,
+        name: "Lajchawe'"
+    },
+    {
+        id: 13,
+        name: "Oxlajunhe'"
+    },
+    {
+        id: 14,
+        name: "Chanhlajunhe'"
+    },
+    {
+        id: 15,
+        name: "Holajunhe'"
+    },
+    {
+        id: 16,
+        name: "Waklajunhe'"
+    },
+    {
+        id: 17,
+        name: "Huklajunhe'"
+    },
+    {
+        id: 18,
+        name: "Wajxaklajunhe'"
+    },
+    {
+        id: 19,
+        name: "B’Alunhlajunhe'"
+    },
+    {
+        id: 20,
+        name: "Junwinak"
+    },
+    {
+        id: 21,
+        name: "Jun xchawinak"
+    },
+    {
+        id: 22,
+        name: "Chab’ xchawinak"
+    },
+    {
+        id: 23,
+        name: "Oxe xchawinak"
+    },
+    {
+        id: 24,
+        name: "Chanhe xchawinak"
+    },
+    {
+        id: 25,
+        name: "Hoye’ xchawinak"
+    },
+    {
+        id: 26,
+        name: "Wake xchawinak"
+    },
+    {
+        id: 27,
+        name: "Huke xchawinak"
+    },
+    {
+        id: 28,
+        name: "Wajxake xchawinak"
+    },
+    {
+        id: 29,
+        name: "B’alunhe xchawinak"
+    },
+    {
+        id: 30,
+        name: "Lajunhe' xchawinak"
+    }
+]
+
+select.forEach(item => {
+    // for (let key in numerosChujEnLetras) { // FOR IN = para ver los index del array y en objeto sale propiedades
+    for (let key of numerosChujEnLetras) { // FOR OF = para ver los valores del array (key.id)
+        
+        // let optionTag = `<option value="${key}">${key}</option>`;
+        // item.insertAdjacentHTML("beforeend", optionTag);
+
+        // console.log(key.id);
+        let optionTag = `<option value="${key.id}">${key.id}</option>`;
+        item.insertAdjacentHTML("beforeend", optionTag);
+    }
+
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = Number(e.currentTarget.numbersList.value);
+
+    if (isNaN(id)) {
+        console.log("Por favor seleccione un numero");
+        input.classList.add("is-invalid");
+        alertDanger.classList.add("invalid-feedback");
+        alertDanger.innerHTML = "Por favor seleccione un número para traducir";
+        return;
+    }
+    input.classList.remove("is-invalid");
+    alertDanger.classList.remove("invalid-feedback");
+    alertDanger.innerHTML = " ";
+    // const findIndex = numerosChujEnLetras.findIndex(item => item.id === id); //findIndex solo encuentra en que posicion esta -> index
+    // console.log("findIndex", findIndex);
+
+    const finalData = numerosChujEnLetras.find(item => item.id === id); // devuelve el valor asigando o lo que queremos encontrar
+    // console.log("FIND", finalData);
+    document.querySelector('#texto').value = finalData.name;
+});
